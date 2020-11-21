9063b3b0f5fa94062b2e15f9c664dedda8f08b8f
AndSzy <aszycher@gmail.com>
2020-11-21 13:15:10 +0000

    Modal Added

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -3,7 +3,34 @@
     <div>
       <h1>My Component</h1>
       <button class="btn btn-primary m-2 float-right" @click="openModal">Open Modal</button>
+      <button class="btn" @click="showModal">Modal</button>
     </div>
+    <b-modal :id="id">
+         <form>
+            <div class="form-control">
+                <label for="chartType">Choose chart type.</label>
+                <select name="chartType" id="chartType" v-model="selectedChartType">
+                    <option value="Line">Line</option>
+                    <option value="Bar">Bar</option>
+                </select>
+            </div>
+            <div class="form-control">
+                <label for="chartColor">Choose chart color.</label>
+                <select name="chartColor" id="chartColor" v-model="selectedColor">
+                    <option value="red">Red</option>
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                </select>
+            </div>
+            <div class="form-control">
+                <label for="chartData">Choose chart dataset.</label>
+                <select name="chartData" id="chartData" v-model="selectedDatasetName">
+                    <option value="data1">Data 1</option>
+                    <option value="data2">Data 2</option>
+                </select>
+            </div>
+        </form>
+    </b-modal>
     <!-- <form>
             <div class="form-control">
                 <label for="chartType">Choose chart type.</label>
@@ -52,47 +79,50 @@ export default {
   },
   props: ["id", "chartType", "color", "datasetName", "dataset"],
   methods: {
+    showModal() {
+        this.$bvModal.show(this.id);
+      },
     openModal() {
       this.$emit("openModal", this.id, this.chartType, this.color, this.datasetName);
     }
   },
-//   data() {
-//     return {
-//       selectedChartType: this.chartType,
-//       selectedColor: this.color,
-//       selectedDatasetName: this.datasetName,
-//     }
-//   },
-//   watch: {
-//     selectedChartType() {
-//         console.log("type changed");
-//       this.$emit(
-//         "editChart",
-//         this.id,
-//         this.selectedChartType,
-//         this.selectedColor,
-//         this.selectedDatasetName
-//       );
-//     },
-//     selectedColor() {
-//       this.$emit(
-//         "editChart",
-//         this.id,
-//         this.selectedChartType,
-//         this.selectedColor,
-//         this.selectedDatasetName
-//       );
-//     },
-//     selectedDatasetName() {
-//       this.$emit(
-//         "editChart",
-//         this.id,
-//         this.selectedChartType,
-//         this.selectedColor,
-//         this.selectedDatasetName
-//       );
-//     },
-//   },
+  data() {
+    return {
+      selectedChartType: this.chartType,
+      selectedColor: this.color,
+      selectedDatasetName: this.datasetName,
+    }
+  },
+  watch: {
+    selectedChartType() {
+        console.log("type changed");
+      this.$emit(
+        "editChart",
+        this.id,
+        this.selectedChartType,
+        this.selectedColor,
+        this.selectedDatasetName
+      );
+    },
+    selectedColor() {
+      this.$emit(
+        "editChart",
+        this.id,
+        this.selectedChartType,
+        this.selectedColor,
+        this.selectedDatasetName
+      );
+    },
+    selectedDatasetName() {
+      this.$emit(
+        "editChart",
+        this.id,
+        this.selectedChartType,
+        this.selectedColor,
+        this.selectedDatasetName
+      );
+    },
+  },
 };
 </script>
 
